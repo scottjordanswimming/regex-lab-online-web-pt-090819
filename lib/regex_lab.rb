df442ee20c92cd5bab8ed6922264b0c97e9e2d9c
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-17 19:06:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,41 @@
+require 'pry'
+
 def starts_with_a_vowel?(word)
+  
+x = word[0]
+y = x.scan(/[aeiouAEIOU]/).join()
+x == y 
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  x = []
+  x << text.scan(/\w+un/).uniq
+  
+  x << text.scan(/\w+ing/).uniq
+  #binding.pry
+x.flatten
 end
 
 def words_five_letters_long(text)
+  arr = []
+  x = text.split(" ")
+  x.each do |word|
+    if word.length == 5
+      arr << word
+    end
+  end
+  arr
+#binding.pry
+      
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+x = text[0]
+y = text[-1]
+y = x.scan(/[aeiouAEIOU]/).join()
+x == y 
 
 end
 
